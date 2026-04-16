9b5c474429d15dc4f4844513b7b7c81295ecc844
mili Arsema <meleymm88@gmail.com>
2026-04-16 03:29:48 -0500

    Built calculator. needs a loop in case of invalid input

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -1,4 +1,40 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class BasicCalculator {
+    public static void main(String[] args) {
+        //build a scanner
+        Scanner fetch = new Scanner (System.in);
+
+        //fetch number input for calculation
+        System.out.println("Enter the first number: ");
+        float firstNumber = fetch.nextFloat();
+
+        System.out.println("Enter the second number: ");
+        float secondNumber = fetch.nextFloat();
+
+        //clearing buffer
+        fetch.nextLine();
+
+        //fetch operator input
+        System.out.println("Please select an option: ");
+        String operator = fetch.nextLine();
+
+        //calculating
+        float result=0;
+        if (operator.equalsIgnoreCase("A")) {
+            result = firstNumber + secondNumber;
+        } else if (operator.equalsIgnoreCase("S")) {
+            result = firstNumber - secondNumber;
+        }else if  (operator.equalsIgnoreCase("M")) {
+            result = firstNumber * secondNumber;
+        }else if (operator.equalsIgnoreCase("D")){
+            result  = firstNumber / secondNumber;
+        } else
+            System.out.println("Invalid input");
+
+        //display result
+        System.out.println("The result is: " + result);
+    }
 }
